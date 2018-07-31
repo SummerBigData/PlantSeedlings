affbbc94934f6e3fa8a418da95d4159251f4e124
Suren Gourapura <osu10171@oakley01.osc.edu>
2018-07-31 18:10:45 -0400

    Automated git push

---
+++ qcnn.sh
@@ -0,0 +1,24 @@
+#PBS -N runCnn7-31
+#PBS -l walltime=40:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=32GB
+#PBS -j oe
+# uncomment if using qsub
+if [ -z "$PBS_O_WORKDIR" ] 
+then
+        echo "PBS_O_WORKDIR not defined"
+else
+        cd $PBS_O_WORKDIR
+        echo $PBS_O_WORKDIR
+fi
+#
+# Setup GPU code
+#module load python/2.7.latest
+source activate local
+
+#
+# This is the command the runs the python script
+
+#python -u icecnn.py $PBS_ARRAYID >& outIceCnn7-2_$PBS_ARRAYID.log
+# h, flip, iters, pseusize
+python -u cnn.py >& outCnn7-31.log
